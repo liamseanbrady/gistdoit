f9e1d0b52593fe52d448b7cf09da4fa7c987cd60
Liam Sean Brady <liamseanbrady@gmail.com>
2015-08-31 11:15:21 +0100

    Require all necessary files

--- lib/gistdoit.rb
+++ lib/gistdoit.rb
@@ -1,3 +1,6 @@
-require 'gistdoit/github'
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'gistdoit/github/client'
+require 'gistdoit/github/network'
+require 'gistdoit/github/user_config'
 require 'gistdoit/gist'
 require 'gistdoit/gist_file'
