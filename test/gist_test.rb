02d3dd9891c0ea86a8390b8a49c89e72cdc9c5e6
Liam Sean Brady <liamseanbrady@gmail.com>
2015-09-02 22:25:56 +0100

    Change tests to account for removal of GistFile

--- test/gist_test.rb
+++ test/gist_test.rb
@@ -4,53 +4,25 @@ require 'gistdoit/gist'
 
 class GistTests < Minitest::Test
   def test_gist_reads_username_from_config_file
-    gist_file_stub = Struct.new(:name, :content) {
-      def name
-        'my_gist.rb'
-      end
-
-      def content
-        'def new_method; end'
-      end
-    }.new
-
     user_config = Struct.new(:user_config) {
       def username
         'liamseanbrady'
       end
     }.new
 
-    gist = Gist.new(gist_file_stub, user_config) 
+    gist = Gist.new(user_config) 
 
     assert_equal('liamseanbrady', gist.github_username)
   end
 
-  def test_gist_has_a_gist_file
-    gist_file_stub = Struct.new(:name, :content) {
-      def name
-        'my_gist.rb'
-      end
-
-      def content
-        'def new_method; end'
-      end
-    }.new
-    gist = Gist.new(gist_file_stub)
-
-    assert_respond_to(gist, :gist_file)
-  end
-
   def test_gist_has_a_summary
-    gist_file_stub = Struct.new(:name, :content) {
-      def name
-        'my_gist.rb'
-      end
-
-      def content
-        'def new_method; end'
+    user_config = Struct.new(:user_config) {
+      def username
+        'liamseanbrady'
       end
     }.new
-    gist = Gist.new(gist_file_stub)
+
+    gist = Gist.new(user_config)
     gist.summary = 'A good summary'
 
     assert_equal('A good summary', gist.summary)
@@ -58,31 +30,27 @@ class GistTests < Minitest::Test
 
 
   def test_gist_has_a_name
-    gist_file_stub = Struct.new(:name, :content) {
-      def name
-        'my_gist.rb'
-      end
-
-      def content
-        'def new_method; end'
+    user_config = Struct.new(:user_config) {
+      def username
+        'liamseanbrady'
       end
     }.new
-    gist = Gist.new(gist_file_stub)
+
+    gist = Gist.new(user_config)
+    gist.name = 'my_gist.rb'
 
     assert_equal('my_gist.rb', gist.name)
   end
 
   def test_gist_has_content
-    gist_file_stub = Struct.new(:name, :content) {
-      def name
-        'my_gist.rb'
-      end
-
-      def content
-        'def new_method; end'
+    user_config = Struct.new(:user_config) {
+      def username
+        'liamseanbrady'
       end
     }.new
-    gist = Gist.new(gist_file_stub)
+
+    gist = Gist.new(user_config)
+    gist.content = 'def new_method; end'
 
     assert_equal('def new_method; end', gist.content)
   end
