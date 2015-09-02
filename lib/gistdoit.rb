0b377986ebe650089b43057b8008315886f579d8
Liam Sean Brady <liamseanbrady@gmail.com>
2015-09-02 21:58:04 +0100

    Require net_http_adapter

--- lib/gistdoit.rb
+++ lib/gistdoit.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'gistdoit/github/client'
-require 'gistdoit/github/network'
+require 'gistdoit/net_http_adapter'
 require 'gistdoit/github/user_config'
 require 'gistdoit/gist'
 require 'gistdoit/gist_file'
