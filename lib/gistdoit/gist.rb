de7d956db528d1afff43117f7873575df1cfdfd9
Liam Sean Brady <liamseanbrady@gmail.com>
2015-10-06 15:01:23 +0100

    Set default argument for keywords apart from user_config to nil

--- lib/gistdoit/gist.rb
+++ lib/gistdoit/gist.rb
@@ -1,43 +1,45 @@
 require 'yaml'
 require 'gistdoit/github/user_config'
 
-class Gist
-  def initialize(name:, content:, summary:, user_config: Github::UserConfig.new)
-    @name = name
-    @content = content
-    @summary = summary
-    @config = user_config
-  end
+module GistDoIt
+  class Gist
+    def initialize(name: nil, content: nil, summary: nil, user_config: Github::UserConfig.new)
+      @name = name
+      @content = content
+      @summary = summary
+      @config = user_config
+    end
 
-  def github_username
-    @config.username
-  end
+    def github_username
+      @config.username
+    end
 
-  def content=(file_contents)
-    @content = file_contents
-  end
+    def content=(file_contents)
+      @content = file_contents
+    end
 
-  def content
-    @content
-  end
+    def content
+      @content
+    end
 
-  def summary
-    @summary
-  end
+    def summary
+      @summary
+    end
 
-  def summary=(text)
-    @summary = text
-  end
+    def summary=(text)
+      @summary = text
+    end
 
-  def name
-    @name
-  end
+    def name
+      @name
+    end
 
-  def name=(text)
-    @name = text
-  end
-  
-  def to_json
-    { "description" => "#{summary}", "public" => false, "files" => { "#{name}" => { "content" => "#{content}" } } }.to_json
+    def name=(text)
+      @name = text
+    end
+    
+    def to_json
+      { "description" => "#{summary}", "public" => false, "files" => { "#{name}" => { "content" => "#{content}" } } }.to_json
+    end
   end
 end
