4e3a622b6c82fa4b9addaceccbcd453b22f6935a
Liam Sean Brady <liamseanbrady@gmail.com>
2015-10-06 15:06:31 +0100

    Add ability to set github username in config file and also check if there is already a username in the config file.
    It seems like there could be a code smell here. GistDoIt::Github::Client is just delegating here. Why so much indirection?

--- lib/gistdoit/github/user_config.rb
+++ lib/gistdoit/github/user_config.rb
@@ -1,7 +1,20 @@
+require 'yaml'
+
 module Github
   class UserConfig
+    # Do we actually need an instance here. Do we require state at any point?
     def initialize(file_path = File.expand_path('~') + '/.gistdoit')
       @file_path = file_path
+    end
+
+    def set_github_username(username)
+      File.open(@file_path, 'a') do |file|
+        file << YAML.dump({ 'github_username' => username })
+      end
+    end
+
+    def has_github_username?
+      YAML.load(File.readlines(@file_path).select { |line| !line.include?('---') }.join).key?('github_username')
     end
 
     def all
